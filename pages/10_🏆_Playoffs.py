96faa110da620258b62019e4b0b4d0a3f3b8f86a
Eric Callahan <ecallahan5@gmail.com>
2023-12-19 11:32:50 -0500

    Semifinal week update

--- pages/10_🏆_Playoffs.py
+++ pages/10_🏆_Playoffs.py
@@ -31,12 +31,13 @@ franchises = api_calls.get_teams()
 keep_cols = ["mfl_id", "division", "franchise_name", "icon_url"]
 df_franchises = pd.DataFrame(franchises)[keep_cols]
 
-champ_df = pd.DataFrame([{"seed": 6, "franchise_id":"0009", "Champ":0.0858,"Runner-Up":0.0981,"3rd place":0.1315,"wc_1":"0.4277","wc_2":"0.0"},\
-                         {"seed": 3, "franchise_id":"0002", "Champ":0.1685,"Runner-Up":0.1398,"3rd place":0.1568,"wc_1":"0.5723","wc_2":"0.0"},\
-                         {"seed": 5, "franchise_id":"0011", "Champ":0.1296,"Runner-Up":0.1584,"3rd place":0.1628,"wc_1":"0.0","wc_2":"0.6729"},\
-                         {"seed": 4, "franchise_id":"0007", "Champ":0.0251,"Runner-Up":0.0572,"3rd place":0.0682,"wc_1":"0.0","wc_2":"0.3271"},\
-                         {"seed": 1, "franchise_id":"0006", "Champ":0.31,"Runner-Up":0.2914,"3rd place":0.214,"wc_1":"0.0","wc_2":"0.0"},\
-                         {"seed": 2, "franchise_id":"0001", "Champ":0.281,"Runner-Up":0.2551,"3rd place":0.2667,"wc_1":"0.0","wc_2":"0.0"}])
+champ_df = pd.DataFrame(
+
+     [{"seed": 4,"franchise_id":"0007","Champ":0.084,"Runner-Up":0.2184,"3rd place":0.1918},
+     {"seed": 3,"franchise_id":"0002","Champ":0.3057,"Runner-Up":0.2245,"3rd place":0.3042},
+     {"seed": 1,"franchise_id":"0006","Champ":0.3453,"Runner-Up":0.3523,"3rd place":0.1502},
+     {"seed": 2,"franchise_id":"0001","Champ":0.265,"Runner-Up":0.2048,"3rd place":0.3538}]
+ )
 
 champ_df = champ_df.merge(df_franchises, left_on = 'franchise_id', right_on = 'mfl_id')
 
@@ -45,20 +46,20 @@ st.subheader('This Week\'s Games', divider=False)
 with st.expander("Click for Previews"):
      with st.container(border = True):
           col1, col2, col3, col4, col5 = st.columns(5)
-          col1.metric("The Gurley Tates", "#6")
-          col2.metric('Win %', "43%")
+          col1.metric("Maize \'N Blue", "#4")
+          col2.metric('Win %', "30%")
           col3.write("Prediction")
-          col3.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
-          col4.metric('Win %', "57%")
-          col5.metric("Brooklyn Big Blue", "#3")
+          col3.image(champ_df.loc[champ_df["seed"] == 1]["icon_url"].values[0])
+          col4.metric('Win %', "70%")
+          col5.metric("The Van Buren Boys", "#1")
      with st.container(border = True):
           col1, col2, col3, col4, col5= st.columns(5)
-          col1.metric("Moneyballers", "#5")
-          col2.metric('Win %', "67%")
+          col1.metric("Brooklyn Big Blue", "#3")
+          col2.metric('Win %', "53%")
           col3.write("Prediction")
-          col3.image(champ_df.loc[champ_df["seed"] == 5]["icon_url"].values[0])
-          col4.metric('Win %', "33%")
-          col5.metric("Maize \'N Blue", "#4")
+          col3.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
+          col4.metric('Win %', "47%")
+          col5.metric("The Uncaught Exceptions", "#2")
 st.divider()
 # # Page Grid layout
 # def make_custom_grid(cols,rows):
@@ -84,38 +85,36 @@ st.divider()
 st.subheader('Bracket', divider=False)
 with st.expander("Click for Bracket"):
 
-     col1, col2, col3 = st.columns(3)
+     col2, col3 = st.columns(2)
 
-     with col1:
-          st.subheader("Wild Card", divider = False)
-          with st.container(border = True):
-               st.image(champ_df.loc[champ_df["seed"] == 6]["icon_url"].values[0])
-          with st.container():
-               st.write("")
-          with st.container(border = True):
-               st.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
-          st.divider()  
-          with st.container(border = True):
-               st.image(champ_df.loc[champ_df["seed"] == 5]["icon_url"].values[0])
-          with st.container():
-               st.write(" ")
-          with st.container(border = True):
-               st.image(champ_df.loc[champ_df["seed"] == 4]["icon_url"].values[0])
+     # with col1:
+     #      st.subheader("Wild Card", divider = False)
+     #      with st.container(border = True):
+     #           st.image(champ_df.loc[champ_df["seed"] == 6]["icon_url"].values[0])
+     #      with st.container():
+     #           st.write("")
+     #      with st.container(border = True):
+     #           st.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
+     #      st.divider()  
+     #      with st.container(border = True):
+     #           st.image(champ_df.loc[champ_df["seed"] == 5]["icon_url"].values[0])
+     #      with st.container():
+     #           st.write(" ")
+     #      with st.container(border = True):
+     #           st.image(champ_df.loc[champ_df["seed"] == 4]["icon_url"].values[0])
           
      with col2:
           st.subheader("Semi Finals", divider = True)
-          with st.container():
-               st.write("")
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
           with st.container(border = True):
                st.image(champ_df.loc[champ_df["seed"] == 2]["icon_url"].values[0])
-          with st.container():
-               st.write("")  
-          with st.container():
-               st.write("")
+          st.divider()  
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 4]["icon_url"].values[0])
           with st.container(border = True):
                st.image(champ_df.loc[champ_df["seed"] == 1]["icon_url"].values[0])
-          with st.container():
-               st.write("")
+
      with col3:    
           st.subheader("Finals", divider = True)
 
@@ -130,52 +129,49 @@ with st.expander("Click for Title Chances"):
 
 st.header("Consolation Bracket", divider=True)
 
-cons_df = pd.DataFrame([ {"seed": 6,"franchise_id":"0005","Extra Pick":0.1377},\
-                         {"seed": 3,"franchise_id":"0004","Extra Pick":0.053},\
-                         {"seed": 5,"franchise_id":"0008","Extra Pick":0.0704},\
-                         {"seed": 4,"franchise_id":"0010","Extra Pick":0.1105},\
-                         {"seed": 1,"franchise_id":"0012","Extra Pick":0.3344},\
-                         {"seed": 2,"franchise_id":"0003","Extra Pick":0.294}])
+cons_df = pd.DataFrame(
+
+[{"seed": 4,"franchise_id":"0010","Extra Pick":0.1786},
+ {"seed": 6,"franchise_id":"0005","Extra Pick":0.2241},
+ {"seed": 1,"franchise_id":"0012","Extra Pick":0.3263},
+ {"seed": 2,"franchise_id":"0003","Extra Pick":0.271}]
+)
 
 cons_df = cons_df.merge(df_franchises, left_on = 'franchise_id', right_on = 'mfl_id')
-
 
 
 st.subheader('Bracket', divider=False)
 with st.expander("Click for Bracket"):
 
-     col1, col2, col3 = st.columns(3)
+     col2, col3 = st.columns(2)
 
-     with col1:
-          st.subheader("Wild Card", divider = False)
-          with st.container(border = True):
-               st.image(cons_df.loc[cons_df["seed"] == 6]["icon_url"].values[0])
-          with st.container():
-               st.write("")
-          with st.container(border = True):
-               st.image(cons_df.loc[cons_df["seed"] == 3]["icon_url"].values[0])
-          st.divider()  
-          with st.container(border = True):
-               st.image(cons_df.loc[cons_df["seed"] == 5]["icon_url"].values[0])
-          with st.container():
-               st.write(" ")
-          with st.container(border = True):
-               st.image(cons_df.loc[cons_df["seed"] == 4]["icon_url"].values[0])
+     # with col1:
+     #      st.subheader("Wild Card", divider = False)
+     #      with st.container(border = True):
+     #           st.image(cons_df.loc[cons_df["seed"] == 6]["icon_url"].values[0])
+     #      with st.container():
+     #           st.write("")
+     #      with st.container(border = True):
+     #           st.image(cons_df.loc[cons_df["seed"] == 3]["icon_url"].values[0])
+     #      st.divider()  
+     #      with st.container(border = True):
+     #           st.image(cons_df.loc[cons_df["seed"] == 5]["icon_url"].values[0])
+     #      with st.container():
+     #           st.write(" ")
+     #      with st.container(border = True):
+     #           st.image(cons_df.loc[cons_df["seed"] == 4]["icon_url"].values[0])
           
      with col2:
           st.subheader("Semi Finals", divider = True)
-          with st.container():
-               st.write("")
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 4]["icon_url"].values[0])
           with st.container(border = True):
                st.image(cons_df.loc[cons_df["seed"] == 2]["icon_url"].values[0])
-          with st.container():
-               st.write("")  
-          with st.container():
-               st.write("")
+          st.divider()  
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 6]["icon_url"].values[0])
           with st.container(border = True):
                st.image(cons_df.loc[cons_df["seed"] == 1]["icon_url"].values[0])
-          with st.container():
-               st.write("")
      with col3:    
           st.subheader("Finals", divider = True)
 
