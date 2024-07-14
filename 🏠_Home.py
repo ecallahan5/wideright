a791871d12e1f8e9fb616f2203e7e8ae1853ade4
Eric Callahan <ecallahan5@gmail.com>
2024-07-14 17:56:31 -0400

    2024 Kickoff

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,35 +1,36 @@
 import streamlit as st
-import api_calls
 import global_vars
 from datetime import datetime
 
-st.title("Welcome to Wide Right")
-st.image("https://www.si.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_298/MTY4MjYzMjM1MjkyODk4NTgx/scott-norwoodgif.webp")
+st.title("Welcome to Wide Right 2024!")
+st.image("https://s.yimg.com/ny/api/res/1.2/QJcrvuxEPZLQX4n3kjaa2g--/YXBwaWQ9aGlnaGxhbmRlcjt3PTI0MDA7aD0xNjI4O2NmPXdlYnA-/https://s.yimg.com/os/creatr-images/2019-02/9c91c250-2653-11e9-bfee-96db584edaa1")
 st.divider()
 
-st.header("Important Dates")
+st.subheader("Updated Content Coming Soon ...")
 
-# Convert the string to a datetime object
-taxi_date = datetime.strptime(global_vars.taxi_dl, '%Y-%m-%d')
-trade_date = datetime.strptime(global_vars.trade_dl, '%Y-%m-%d')
+# st.header("Important Dates")
 
-# Format the datetime object as 'Month Day, Year'
-formatted_taxi_dl = taxi_date.strftime('%B %d, %Y')
-formatted_trade_dl = trade_date.strftime('%B %d, %Y')
+# # Convert the string to a datetime object
+# taxi_date = datetime.strptime(global_vars.taxi_dl, '%Y-%m-%d')
+# trade_date = datetime.strptime(global_vars.trade_dl, '%Y-%m-%d')
 
-# Days Left
-current_date = datetime.now()
-taxi_days_left = (taxi_date - current_date).days +1
-trade_days_left = (trade_date - current_date).days +1
+# # Format the datetime object as 'Month Day, Year'
+# formatted_taxi_dl = taxi_date.strftime('%B %d, %Y')
+# formatted_trade_dl = trade_date.strftime('%B %d, %Y')
 
-if current_date > taxi_date:
-    st.subheader("Taxi Claims are closed!")
-else:
-    st.subheader("Taxi Claims end on "+str(formatted_taxi_dl))
-    st.caption(str(taxi_days_left)+" days left!")
+# # Days Left
+# current_date = datetime.now()
+# taxi_days_left = (taxi_date - current_date).days +1
+# trade_days_left = (trade_date - current_date).days +1
 
-if current_date > trade_date:
-    st.subheader("Trades and Extenstions are closed!")
-else:
-    st.subheader("Trades and Extensions end on "+str(formatted_trade_dl))
-    st.caption(str(trade_days_left)+" days left!")
+# if current_date > taxi_date:
+#     st.subheader("Taxi Claims are closed!")
+# else:
+#     st.subheader("Taxi Claims end on "+str(formatted_taxi_dl))
+#     st.caption(str(taxi_days_left)+" days left!")
+
+# if current_date > trade_date:
+#     st.subheader("Trades and Extenstions are closed!")
+# else:
+#     st.subheader("Trades and Extensions end on "+str(formatted_trade_dl))
+#     st.caption(str(trade_days_left)+" days left!")
