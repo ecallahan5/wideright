df189528c130694d127ba061b590c1bd288f3081
Eric Callahan <ecallahan5@gmail.com>
2024-08-15 18:04:58 -0400

    hotfix draft table

--- pages/1_👶_Draft Picks.py
+++ pages/1_👶_Draft Picks.py
@@ -10,10 +10,10 @@ st.divider()
 # Get the draft picks 
 picks = functions.bq_query("SELECT * FROM `mfl-374514.dbt_production.fct_draft_picks`")
 picks_df = pd.DataFrame(picks)
-picks_df[["year", "round_num"]] = picks_df[["year", "round_num"]].apply(pd.to_numeric)
+picks_df[["year", "round_num", "pick_num"]] = picks_df[["year", "round_num", "pick_num"]].apply(pd.to_numeric, downcast="integer")
 
 # Get the list of teams
-teams = functions.bq_query("SELECT franchise_id, name, division, icon FROM `mfl-374514.dbt_production.fct_franchises`")
+teams = functions.bq_query("SELECT franchise_id, franchise_name, division, icon FROM `mfl-374514.dbt_production.fct_franchises`")
 teams_df = pd.DataFrame(teams)
 
 picks_clean = picks_df.drop(["pick_owner"], axis = 1).rename(columns={"year": "Year", "round_num": "Round", "pick_num": "Pick"}).sort_values(["Year", "Round"])
@@ -44,7 +44,7 @@ id_list = teams_df["franchise_id"].unique()
 i = 0
 for row_num in list(range(grid_row_count)):
     for col_num in list(range(grid_col_count)):
-        mygrid[row_num][col_num].subheader(teams_df.loc[teams_df["franchise_id"] == id_list[i]]["name"].values[0], divider = True)
+        mygrid[row_num][col_num].subheader(teams_df.loc[teams_df["franchise_id"] == id_list[i]]["franchise_name"].values[0], divider = True)
         mygrid[row_num][col_num].image(teams_df.loc[teams_df["franchise_id"] == id_list[i]]["icon"].values[0])
         mygrid[row_num][col_num].table(picks_clean.loc[(picks_df["pick_owner"] == id_list[i]) & (picks_df["year"].isin(year_select)) & (picks_df["round_num"].isin(round_select))])
 
