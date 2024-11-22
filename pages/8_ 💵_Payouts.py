416dcd6f2f51e82a964ea22d9545dff6d641d767
Eric Callahan <ecallahan5@gmail.com>
2024-11-22 08:44:07 -0500

    archiving ext and taxi pages

--- pages/8_ 💵_Payouts.py
+++ pages/8_ 💵_Payouts.py
@@ -10,7 +10,7 @@ import functions
 st.set_page_config(layout="wide")
 st.title("Payouts")
 
-st.subheader("Seasons to Date Winnings", divider=True)
+st.subheader("Season to Date Winnings", divider=True)
 
 payouts = functions.bq_query(f"SELECT b.franchise_name as `Franchise Name`, sum(a.payout) as Winnings \
                                FROM `mfl-374514.external.payouts` a\
@@ -20,6 +20,47 @@ payouts = functions.bq_query(f"SELECT b.franchise_name as `Franchise Name`, sum(
 payouts_df = pd.DataFrame(payouts)
 
 st.dataframe(payouts_df, hide_index=True, use_container_width = True)
+st.divider()
+st.subheader("Regular Season - Up for Grabs", divider=True)
+
+after_wk = functions.bq_query("SELECT recent_completed_week FROM `mfl-374514.dbt_production.dim_current_week`")[0]["recent_completed_week"]
+
+div_probs = functions.bq_query(f"SELECT b.franchise_name as `Franchise Name`, b.division, b.icon, a.win_division as `Division Title Chance` \
+                               FROM `mfl-374514.dbt_production.fct_reg_season_model` a\
+                               join `mfl-374514.dbt_production.dim_franchises` b \
+                                   on a.franchise_id = b.franchise_id \
+                               where after_week = {after_wk}  ")
+div_probs_df = pd.DataFrame(div_probs)
+
+col1, col2 = st.columns(2, gap = "large")
+
+with col1:
+    # grid_row_count = 6
+    # grid_col_count = 1
+
+    # mygrid = global_vars.make_grid(grid_row_count,grid_col_count)
+
+    # for col_num, division in enumerate(div_probs_df["division"].unique()):
+    # # Filter and sort the DataFrame for the specific division
+    #     div_df = div_probs_df.loc[div_probs_df["division"] == division].sort_values(by='Division Title Chance', ascending=False).reset_index()
+    #     # Get the first row for the division
+    #     top_team = div_df.iloc[0]
+    #     # Display the division name as a subheader in the appropriate column
+    #     mygrid[0][col_num].subheader(str(division)+" Title Chances - $50", divider=True)
+    #     mygrid[0][col_num].metric(
+    #         label=top_team["Franchise Name"],  
+    #         value=f"{top_team['Division Title Chance']:.0%}",  # Format as a percentage
+    #     )       
+
+with col2:
+    prizes_left = 75
+    weekly_pot = 5
+    st.metric("Weekly Prizes Left", f"${prizes_left:,.0f}")
+    st.metric("This Week's Big Win Pot", f"${weekly_pot:,.0f}")
+
+
+        
+
 ###########################################################################
 
 # st.subheader("Regular Season Standings", divider=True)
@@ -48,34 +89,6 @@ st.dataframe(payouts_df, hide_index=True, use_container_width = True)
 #      'Winner' : [ "The Uncaught Exceptions", "Moneyballers", "Brooklyn Big Blue", ]
 #      })
 # st.dataframe(extras, hide_index=True, use_container_width = True)
-
-# # Import Schedules
-# schedule = functions.get_schedule()
-# results_dict =schedule["matchups_by_week"]
-
-# #Define Current, Next Weeks
-# names = global_vars.extract_values(schedule, 'current_week')
-# current_wk = round(float(names[0]))
-
-# # Get game scores
-# scores = []
-
-# for week, results in results_dict.items():
-#     if int(week) < current_wk:
-#         for matchup in results:
-#             home_score = {}
-#             away_score = {}
-#             home_score["week"] = int(week)
-#             home_score["franchise"] = matchup["home_franchise"] 
-#             home_score["score"] = float(matchup["home_franchise_score"])
-#             away_score["week"] = int(week)
-#             away_score["franchise"] = matchup["away_franchise"] 
-#             away_score["score"] = float(matchup["away_franchise_score"])
-#             scores.append(home_score)
-#             scores.append(away_score)
-
-# scores_df = pd.DataFrame(scores)
-# results_df = pd.DataFrame([])
 
 # pot = 15
 # for week in range(1,current_wk):
