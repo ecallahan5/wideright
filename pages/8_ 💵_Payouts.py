9c42dadf1688a54b12876ce33e858169bbeb6caf
Eric Callahan <ecallahan5@gmail.com>
2024-12-04 17:48:39 -0500

    updates wk 14 cash amounts

--- pages/8_ 💵_Payouts.py
+++ pages/8_ 💵_Payouts.py
@@ -71,8 +71,8 @@ with col2:
           )       
 with col3:
     st.subheader("Weekly Side Games", divider=True)
-    prizes_left = 55
-    weekly_pot = 10
+    prizes_left = 35
+    weekly_pot = 15
     st.metric("Weekly Prizes Left", f"${prizes_left:,.0f}")
     st.metric("This Week's Big Win Pot", f"${weekly_pot:,.0f}")
 
