f26b67cc2d32fcc9ef3b178fc44787842b6bfe10
Eric Callahan <ecallahan5@gmail.com>
2024-12-18 08:55:28 -0500

    playoff site

--- pages/10_🏆_Playoffs.py
+++ pages/10_🏆_Playoffs.py
@@ -8,8 +8,8 @@ import global_vars
 import functions
 import plotly.express as px
 import plotly.graph_objects as go
-
-st.image(global_vars.coming_soon)
+st.set_page_config(layout="wide")
+# st.image(global_vars.coming_soon)
 
 # Things to use during regular season
 
@@ -24,174 +24,178 @@ st.image(global_vars.coming_soon)
 #      'Team': [)
 # st.dataframe(seeds, hide_index=True, use_container_width = True)
 
-# st.set_page_config(layout="wide")
-# st.title("Playoffs ")
-# st.header("Championship Bracket", divider=True)
 
-# # Get the list of teams
-# franchises = functions.get_teams()
-# keep_cols = ["mfl_id", "division", "franchise_name", "icon_url"]
-# df_franchises = pd.DataFrame(franchises)[keep_cols]
+st.title("Playoffs ")
+st.header("Championship Bracket", divider=True)
 
-# champ_df = pd.DataFrame(
+# Get the list of teams
+teams = functions.bq_query("SELECT franchise_id, franchise_name, division, icon FROM `mfl-374514.dbt_production.dim_franchises`")
+teams_df = pd.DataFrame(teams)
 
-#      [{"seed": 4,"franchise_id":"0007","Champ":0.2346,"Runner-Up":0.7654,"3rd place":0},
-#      {"seed": 3,"franchise_id":"0002","Champ":0,"Runner-Up":0,"3rd place":0.4471},
-#      {"seed": 1,"franchise_id":"0006","Champ":0.7654,"Runner-Up":0.2346,"3rd place":0},
-#      {"seed": 2,"franchise_id":"0001","Champ":0,"Runner-Up":0,"3rd place":0.5529}]
-#  )
+champ_df = pd.DataFrame(
 
-# champ_df = champ_df.merge(df_franchises, left_on = 'franchise_id', right_on = 'mfl_id')
+     [{"seed": 4,"franchise_id":"0008","Champ":0.1846,"Runner-Up":0.3042,"3rd place":0.2255},
+     {"seed": 3,"franchise_id":"0002","Champ":0.4189,"Runner-Up":0.2389,"3rd place":0.2215},
+     {"seed": 1,"franchise_id":"0001","Champ":0.2216,"Runner-Up":0.2896,"3rd place":0.2227},
+    {"seed": 5,"franchise_id":"0003","Champ":0,"Runner-Up":0,"3rd place":0},
+     {"seed": 6,"franchise_id":"0008","Champ":0,"Runner-Up":0,"3rd place":0},
+     {"seed": 2,"franchise_id":"0009","Champ":0.1749,"Runner-Up":0.1673,"3rd place":0.3303}]
+ )
 
-# st.subheader('This Week\'s Games', divider=False)
+champ_df = champ_df.merge(teams_df, on = 'franchise_id')
 
-# with st.expander("Click for Previews"):
-#      with st.container(border = True):
-#           col1, col2, col3, col4, col5 = st.columns(5)
-#           col1.metric("Maize \'N Blue", "#4")
-#           col2.metric('Win %', "23%")
-#           col3.write("Prediction")
-#           col3.image(champ_df.loc[champ_df["seed"] == 2]["icon_url"].values[0])
-#           col4.metric('Win %', "77%")
-#           col5.metric("The Uncaught Exceptions", "#2")
-#      with st.container(border = True):
-#           col1, col2, col3, col4, col5= st.columns(5)
-#           col1.metric("Brooklyn Big Blue", "#3")
-#           col2.metric('Win %', "53%")
-#           col3.write("Prediction")
-#           col3.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
-#           col4.metric('Win %', "47%")
-#           col5.metric("The Van Buren Boys", "#1")
-# st.divider()
-# # # Page Grid layout
-# # def make_custom_grid(cols,rows):
-# #     grid = [0]*cols
-# #     for i in range(cols):
-# #         with st.container():
-# #             grid[i] = st.columns(rows)
-# #     return grid
+st.subheader('This Week\'s Games', divider=False)
 
-# # grid_row_count = 6
-# # grid_col_count = 3
-
-# # mygrid = make_custom_grid(grid_row_count,grid_col_count)
-
-# # mygrid[0][0].subheader("Wild Card", divider = True)
-# # mygrid[0][1].subheader("Semi Finals", divider = True)
-# # mygrid[0][2].subheader("Finals", divider = True)
-
-# # for row_num in list(range(grid_row_count)):
-# #     for col_num in list(range(grid_col_count)):
-          
-# #           mygrid[row_num][col_num].write("penus")
-# st.subheader('Bracket', divider=False)
-# with st.expander("Click for Bracket"):
-
-#      col2, col3 = st.columns(2)
-
-#      # with col1:
-#      #      st.subheader("Wild Card", divider = False)
-#      #      with st.container(border = True):
-#      #           st.image(champ_df.loc[champ_df["seed"] == 6]["icon_url"].values[0])
-#      #      with st.container():
-#      #           st.write("")
-#      #      with st.container(border = True):
-#      #           st.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
-#      #      st.divider()  
-#      #      with st.container(border = True):
-#      #           st.image(champ_df.loc[champ_df["seed"] == 5]["icon_url"].values[0])
-#      #      with st.container():
-#      #           st.write(" ")
-#      #      with st.container(border = True):
-#      #           st.image(champ_df.loc[champ_df["seed"] == 4]["icon_url"].values[0])
-          
-#      with col2:
-#           st.subheader("Semi Finals", divider = True)
-#           with st.container(border = True):
-#                st.image(champ_df.loc[champ_df["seed"] == 3]["icon_url"].values[0])
-#           with st.container(border = True):
-#                st.image(champ_df.loc[champ_df["seed"] == 2]["icon_url"].values[0])
-#           st.divider()  
-#           with st.container(border = True):
-#                st.image(champ_df.loc[champ_df["seed"] == 4]["icon_url"].values[0])
-#           with st.container(border = True):
-#                st.image(champ_df.loc[champ_df["seed"] == 1]["icon_url"].values[0])
-
-#      with col3:    
-#           st.subheader("Finals", divider = True)
-#           with st.container(border = True):
-#                st.image(champ_df.loc[champ_df["seed"] == 4]["icon_url"].values[0])
-#           with st.container(border = True):
-#                st.image(champ_df.loc[champ_df["seed"] == 2]["icon_url"].values[0])
-
+with st.expander("Click for Previews"):
+     with st.container(border = True):
+          col1, col2, col3, col4, col5 = st.columns(5)
+          col1.metric("Hidden Talents", "#4")
+          col2.metric('Win %', "48%")
+          col3.write("Prediction")
+          col3.image(champ_df.loc[champ_df["seed"] == 1]["icon"].values[0])
+          col4.metric('Win %', "52%")
+          col5.metric("The Uncaught Exceptions", "#1")
+     with st.container(border = True):
+          col1, col2, col3, col4, col5= st.columns(5)
+          col1.metric("Brooklyn Big Blue", "#3")
+          col2.metric('Win %', "66%")
+          col3.write("Prediction")
+          col3.image(champ_df.loc[champ_df["seed"] == 3]["icon"].values[0])
+          col4.metric('Win %', "34%")
+          col5.metric("The Gurley Tates", "#2")
 
 # st.divider()
+# # Page Grid layout
+# def make_custom_grid(cols,rows):
+#     grid = [0]*cols
+#     for i in range(cols):
+#         with st.container():
+#             grid[i] = st.columns(rows)
+#     return grid
 
-# st.subheader('Title Chances', divider=False)
-# with st.expander("Click for Title Chances"):
-#      df_ch = champ_df.rename(columns={"franchise_name" : "Team", "Champ" : "Probability"})
-#      st.bar_chart(df_ch, x='Team', y='Probability')
+# grid_row_count = 6
+# grid_col_count = 3
 
+# mygrid = make_custom_grid(grid_row_count,grid_col_count)
 
-# st.header("Consolation Bracket", divider=True)
+# mygrid[0][0].subheader("Wild Card", divider = True)
+# mygrid[0][1].subheader("Semi Finals", divider = True)
+# mygrid[0][2].subheader("Finals", divider = True)
 
-# cons_df = pd.DataFrame(
-
-# [{"seed": 4,"franchise_id":"0010","Extra Pick":0},
-#  {"seed": 6,"franchise_id":"0005","Extra Pick":0},
-#  {"seed": 1,"franchise_id":"0012","Extra Pick":0.5169},
-#  {"seed": 2,"franchise_id":"0003","Extra Pick":0.4831}]
-# )
-
-# cons_df = cons_df.merge(df_franchises, left_on = 'franchise_id', right_on = 'mfl_id')
-
-
-# st.subheader('Bracket', divider=False)
-# with st.expander("Click for Bracket"):
-
-#      col2, col3 = st.columns(2)
-
-#      # with col1:
-#      #      st.subheader("Wild Card", divider = False)
-#      #      with st.container(border = True):
-#      #           st.image(cons_df.loc[cons_df["seed"] == 6]["icon_url"].values[0])
-#      #      with st.container():
-#      #           st.write("")
-#      #      with st.container(border = True):
-#      #           st.image(cons_df.loc[cons_df["seed"] == 3]["icon_url"].values[0])
-#      #      st.divider()  
-#      #      with st.container(border = True):
-#      #           st.image(cons_df.loc[cons_df["seed"] == 5]["icon_url"].values[0])
-#      #      with st.container():
-#      #           st.write(" ")
-#      #      with st.container(border = True):
-#      #           st.image(cons_df.loc[cons_df["seed"] == 4]["icon_url"].values[0])
+# for row_num in list(range(grid_row_count)):
+#     for col_num in list(range(grid_col_count)):
           
-#      with col2:
-#           st.subheader("Semi Finals", divider = True)
+#           mygrid[row_num][col_num].write("penus")
+st.subheader('Bracket', divider=False)
+with st.expander("Click for Bracket"):
+
+     col1, col2, col3 = st.columns(3)
+
+     with col1:
+          st.subheader("Wild Card", divider = True)
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 6]["icon"].values[0])
+          with st.container():
+               st.write("")
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 3]["icon"].values[0])
+          st.divider()  
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 5]["icon"].values[0])
+          with st.container():
+               st.write(" ")
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 4]["icon"].values[0])
+          
+     with col2:
+          st.subheader("Semi Finals", divider = True)
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 3]["icon"].values[0])
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 2]["icon"].values[0])
+          st.divider()  
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 4]["icon"].values[0])
+          with st.container(border = True):
+               st.image(champ_df.loc[champ_df["seed"] == 1]["icon"].values[0])
+
+     with col3:    
+          st.subheader("Finals", divider = True)
 #           with st.container(border = True):
-#                st.image(cons_df.loc[cons_df["seed"] == 4]["icon_url"].values[0])
+#                st.image(champ_df.loc[champ_df["seed"] == 4]["icon"].values[0])
 #           with st.container(border = True):
-#                st.image(cons_df.loc[cons_df["seed"] == 2]["icon_url"].values[0])
-#           st.divider()  
+#                st.image(champ_df.loc[champ_df["seed"] == 2]["icon"].values[0])
+
+
+st.divider()
+
+st.subheader('Title Chances', divider=False)
+with st.expander("Click for Title Chances"):
+    df_ch = champ_df.rename(columns={"franchise_name" : "Team", "Champ" : "Probability"})
+    st.bar_chart(df_ch, x='Team', y='Probability')
+
+
+st.header("Consolation Bracket", divider=True)
+
+cons_df = pd.DataFrame(
+
+[{"seed": 6,"franchise_id":"0004","Extra Pick":0.1154},
+ {"seed": 4,"franchise_id":"0007","Extra Pick":0.1739},
+ {"seed": 1,"franchise_id":"0006","Extra Pick":0.4147},
+ {"seed": 3,"franchise_id":"0005","Extra Pick":0},
+ {"seed": 5,"franchise_id":"0010","Extra Pick":0},
+ {"seed": 2,"franchise_id":"0012","Extra Pick":0.2960}]
+)
+
+cons_df = cons_df.merge(teams_df, on = 'franchise_id')
+
+
+st.subheader('Bracket', divider=False)
+with st.expander("Click for Bracket"):
+
+     col1, col2, col3 = st.columns(3)
+
+     with col1:
+          st.subheader("Wild Card", divider = True)
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 6]["icon"].values[0])
+          with st.container():
+               st.write("")
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 3]["icon"].values[0])
+          st.divider()  
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 5]["icon"].values[0])
+          with st.container():
+               st.write(" ")
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 4]["icon"].values[0])
+          
+     with col2:
+          st.subheader("Semi Finals", divider = True)
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 4]["icon"].values[0])
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 2]["icon"].values[0])
+          st.divider()  
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 6]["icon"].values[0])
+          with st.container(border = True):
+               st.image(cons_df.loc[cons_df["seed"] == 1]["icon"].values[0])
+     with col3:    
+          st.subheader("Finals", divider = True)
 #           with st.container(border = True):
-#                st.image(cons_df.loc[cons_df["seed"] == 6]["icon_url"].values[0])
+#                st.image(cons_df.loc[cons_df["seed"] == 2]["icon"].values[0])
 #           with st.container(border = True):
-#                st.image(cons_df.loc[cons_df["seed"] == 1]["icon_url"].values[0])
-#      with col3:    
-#           st.subheader("Finals", divider = True)
-#           with st.container(border = True):
-#                st.image(cons_df.loc[cons_df["seed"] == 2]["icon_url"].values[0])
-#           with st.container(border = True):
-#                st.image(cons_df.loc[cons_df["seed"] == 1]["icon_url"].values[0])
+#                st.image(cons_df.loc[cons_df["seed"] == 1]["icon"].values[0])
 
 
 # st.divider()
 
-# st.subheader('Extra Pick Chances', divider=False)
-# with st.expander("Click for Extra Pick Chances"):
-#      df_ch = cons_df.rename(columns={"franchise_name" : "Team", "Extra Pick" : "Probability"})
-#      st.bar_chart(df_ch, x='Team', y='Probability')
+st.subheader('Extra Pick Chances', divider=False)
+with st.expander("Click for Extra Pick Chances"):
+     df_ch = cons_df.rename(columns={"franchise_name" : "Team", "Extra Pick" : "Probability"})
+     st.bar_chart(df_ch, x='Team', y='Probability')
 
 # ##################################################################
 
