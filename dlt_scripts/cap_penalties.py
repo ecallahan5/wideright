34fae34b44d6b213f8275393464beda1fb6360ed
Eric Callahan <ecallahan5@gmail.com>
2025-07-28 16:54:40 -0400

    add cap penalties endpoint

---
+++ dlt_scripts/cap_penalties.py
@@ -0,0 +1,33 @@
+import os
+import dlt
+from dlt.sources.helpers import requests
+from .common import _create_auth_headers, create_dlt_pipeline
+from global_vars import host, league_id, league_year
+
+
+@dlt.resource(write_disposition="replace")
+def league_resource(mfl_api_key=dlt.secrets.value):
+    # headers = _create_auth_headers(mfl_api_key) # Not used by MFL API if key is in URL
+    # print(headers)
+
+    # make an api call here
+    url = f"https://{host}/{league_year}/export?TYPE=salaryAdjustments&L={league_id}&APIKEY={mfl_api_key}&JSON=1"
+    response = requests.get(url)
+    response.raise_for_status()
+    yield response.json()
+
+@dlt.source
+def league_source(resource_func):
+    yield resource_func
+
+
+if __name__ == "__main__":
+    force_create = os.getenv("FORCE_DLT_CREATE_MODE", "false").lower() == "true"
+    create_dlt_pipeline(
+        pipeline_name='mfl_league',
+        dataset_name='league',
+        resource_func=league_resource,
+        source_func=league_source,
+        force_create_mode=force_create
+
+    )
