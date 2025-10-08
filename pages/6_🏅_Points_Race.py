6f0eb90d6f98daebf11ad91309db62e765eafc7f
Eric Callahan <ecallahan5@gmail.com>
2025-10-08 08:59:49 -0400

    added WoW pts race

--- pages/6_🏅_Points_Race.py
+++ pages/6_🏅_Points_Race.py
@@ -9,25 +9,28 @@ st.title("Points Race")
 st.divider()
 
 after_wk = functions.bq_query("SELECT recent_completed_week FROM `mfl-374514.dbt_production.dim_current_week`")[0]["recent_completed_week"]
-after_wk = int(after_wk)  # Convert to integer
+after_wk = int(after_wk)  
 last_wk = functions.bq_query("SELECT recent_completed_week - 1 as last_week FROM `mfl-374514.dbt_production.dim_current_week`")[0]["last_week"]
+after_wk = int(after_wk)
 
-top_pts_probs = functions.bq_query(f"SELECT franchise_name as `Team`, top_pts, icon \
-                                   FROM `mfl-374514.dbt_production.fct_reg_season_model` a \
-                                   join `mfl-374514.dbt_production.dim_franchises` b \
-                                   on a.franchise_id = b.franchise_id \
-                                   where after_week = {after_wk} \
-                                   order by top_pts desc \
-                                   limit 3  ")
+
+top_pts_probs = functions.bq_query(f"""
+    SELECT 
+        franchise_name as `Team`, 
+        curr.top_pts as top_pts, 
+        icon,
+        prev.top_pts as lw_top_pts
+    FROM `mfl-374514.dbt_production.fct_reg_season_model` curr
+    JOIN `mfl-374514.dbt_production.dim_franchises` b 
+        ON curr.franchise_id = b.franchise_id 
+    LEFT JOIN `mfl-374514.dbt_production.fct_reg_season_model` prev
+        ON curr.franchise_id = prev.franchise_id 
+        AND prev.after_week = {last_wk}
+    WHERE curr.after_week = {after_wk} 
+    ORDER BY curr.top_pts DESC 
+    LIMIT 3
+""")
 top_pts_probs_df = pd.DataFrame(top_pts_probs)
-
-lw_probs = functions.bq_query(f"SELECT franchise_name as `Team`, top_pts as lw_top_pts \
-                                   FROM `mfl-374514.dbt_production.fct_reg_season_model` a \
-                                   join `mfl-374514.dbt_production.dim_franchises` b \
-                                   on a.franchise_id = b.franchise_id \
-                                   where after_week = {last_wk} \
-                                   order by top_pts desc  ")
-lw_probs_df = pd.DataFrame(lw_probs)
 
 pts = functions.bq_query("SELECT franchise_name as `Franchise Name`, points_for as `Total Points` \
                                FROM `mfl-374514.dbt_production.dim_standings` a\
@@ -44,8 +47,6 @@ with col1:
  
 with col2: 
     st.subheader("Top 3 Likeliest Winners", divider=True)
-#     lw_prob_df = merged_df1.loc[merged_df1["current_wk"] == chosen_wk - 1][["franchise_name", "icon_url", "current_wk", "top_pts"]]\
-#         .rename(columns={"franchise_name": "Team", "top_pts" : "lw_top_pts"}).drop(["icon_url", 'current_wk'], axis = 1)
     grid_row_count = 3
     grid_col_count = 2
 
@@ -54,11 +55,10 @@ with col2:
         helmet = top_pts_probs_df["icon"].values[row_num]
         team = top_pts_probs_df["Team"].values[row_num]
         prob = float(top_pts_probs_df["top_pts"].values[row_num])*100
-        # lw_prob = float(lw_probs_df["lw_top_pts"].values[row_num])*100
-        # wow = prob - lw_prob
+        lw_prob = float(top_pts_probs_df["lw_top_pts"].values[row_num])*100
+        wow = prob - lw_prob
         mygrid[row_num][0].image(helmet)
-        # mygrid[row_num][1].metric(team,"{0:.2f}%".format(prob), "{0:.2f}%".format(wow))
-        mygrid[row_num][1].metric(team,"{0:.2f}%".format(prob))
+        mygrid[row_num][1].metric(team,"{0:.2f}%".format(prob), "{0:.2f}%".format(wow))
 
 ######################################
 
